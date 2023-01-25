940961c5c478c70d4790fbba605aeb243a0528a5
Gede Darma Yuda <gedheyuda398@gmail.com>
2023-01-25 09:37:12 +0700

    fungsi obat commit

--- obat/urls.py
+++ obat/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('', views.obat_list, name="obat_list"),
     re_path(r'^(?P<kode_obat>\d+)/update/$', views.update_obat, name="update_obat"),
     re_path(r'^(?P<kode_obat>\d+)/delete/$', views.delete_obat, name="delete_obat"),
+    
+    # Fungsi Obat
+    path('f_crete/', views.create_f_obat, name='create_f_obat'),
+    path('f_obat_list/', views.obat_f_list, name='f_obat_list'),
+    re_path(r'^(?P<nof_obat>\d+)/f_update/$', views.update_f_obat, name="update_f_obat"),
+    re_path(r'^(?P<nof_obat>\d+)/f_delete/$', views.delete_f_obat, name="delete_f_obat"),
 ]
--- obat/views.py
+++ obat/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 # Create your views here.
 
 # Obat
+
+
 def create_obat(request):
     if request.POST:
         form = FormObat(request.POST)
@@ -20,13 +22,13 @@ def create_obat(request):
         form = FormObat()
         obat = {
             'form': form
-            }
+        }
         return render(request, 'obat/tambah_obat.html', obat)
 
 
 def obat_list(request):
     obat = Obat.objects.all()
-    return render(request, "obat/list_obat.html",{ "obats": obat,})
+    return render(request, "obat/list_obat.html", {"obats": obat, })
 
 
 def update_obat(request, kode_obat):
@@ -42,7 +44,7 @@ def update_obat(request, kode_obat):
         obat = {
             'form': form,
             'obats': obats
-            }
+        }
         return render(request, 'obat/edit_obat.html', obat)
 
 
@@ -53,6 +55,8 @@ def delete_obat(request, kode_obat):
     return render(request, 'obat/list_obat.html')
 
 # Fungsi Obat
+
+
 def create_f_obat(request):
     if request.POST:
         form = FormFObat(request.POST)
@@ -68,34 +72,34 @@ def create_f_obat(request):
         form = FormFObat()
         f_obat = {
             'form': form
-            }
+        }
         return render(request, 'fungsi_obat/tambah_f_obat.html', f_obat)
 
 
 def obat_f_list(request):
     f_obat = Fungsi_Obat.objects.all()
-    return render(request, "fungsi_obat/list_f_obat.html",{ "f_obats": f_obat,})
+    return render(request, "fungsi_obat/list_f_obat.html", {"f_obats": f_obat, })
 
 
-def update_f_obat(request, kode_obat):
-    obats = Obat.objects.get(id=kode_obat)
+def update_f_obat(request, nof_obat):
+    fobats = Fungsi_Obat.objects.get(id=nof_obat)
     if request.method == 'POST':
-        form = FormObat(request.POST, instance=obats)
+        form = FormObat(request.POST, instance=fobats)
         if form.is_valid():
             form.save()
             messages.success(request, 'Data berhasil diubah')
-            return redirect('obat:update_obat', kode_obat=kode_obat)
+            return redirect('obat:update_obat', nof_obat=nof_obat)
     else:
-        form = FormObat(instance=obats)
-        obat = {
+        form = FormFObat(instance=fobats)
+        fobats = {
             'form': form,
-            'obats': obats
-            }
-        return render(request, 'obat/edit_obat.html', obat)
+            'fobats': fobats
+        }
+        return render(request, 'fungsi_obat/edit_f_obat.html', fobats)
 
 
-def delete_obat(request, kode_obat):
-    obats = Obat.objects.filter(id=kode_obat)
-    obats.delete()
+def delete_f_obat(request, kode_obat):
+    fobats = Fungsi_Obat.objects.filter(id=kode_obat)
+    fobats.delete()
     messages.success(request, "Data terhapus")
-    return render(request, 'obat/list_obat.html')
+    return render(request, 'fungsi_obat/list_f_obat.html')
